839abc113350e6cfb29f3932514013e2c9360559
pratikgosavii <pratikgosavi654@gmail.com>
2023-03-18 07:12:50 +0000

    sdsd

--- transport/wsgi.py
+++ transport/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for transport project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# assuming your Django settings file is at
+# '/home/myusername/mysite/mysite/settings.py'
+path = '/home/sahanigoods/transport_management'
+if path not in sys.path:
+     sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'transport.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'transport.settings')
-
 application = get_wsgi_application()
