3f8cf2f6990707a301f49f7aec9267ee3f1fc995
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2023-04-03 04:21:38 +0530

    D

--- transport/views.py
+++ transport/views.py
@@ -21,7 +21,7 @@ def dashboard(request):
     user_count = User.objects.all().count()
 
     if request.user.is_superuser:
-        builty_data = builty.objects.all().order_by(Substr('builty_no',4))
+        builty_data = builty.objects.all().order_by('DC_date)
     else:
         builty_data = None
 
