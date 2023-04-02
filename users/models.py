d03471bfe5e03c21d4c9648d7fa68de9b8984968
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2023-04-02 15:29:35 +0530

    d

--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ from store.models import *
 class User(AbstractUser):
     
     company = models.ForeignKey(company, on_delete=models.CASCADE, blank = True, null = True)
+    city = models.ForeignKey(station, on_delete=models.CASCADE, blank = True, null = True)
     password_r = models.CharField(max_length=50, null = True, blank = True)
     date_joined =  models.DateField(default=datetime.now, blank=True, null=True)
 
