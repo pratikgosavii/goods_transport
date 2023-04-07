300cd790762238efc8660a86dd6e29d31a96ffd7
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2023-04-07 20:36:33 +0530

    sd

--- transport/views.py
+++ transport/views.py
@@ -23,77 +23,75 @@ def dashboard(request):
     builty_count = builty.objects.all().count()
     truck_count = truck_details.objects.all().count()
     user_count = User.objects.all().count()
+    data = None
 
     if request.user.is_superuser:
         builty_data = builty.objects.all().order_by('-id')
-    else:
-        builty_data = None
+        
+        total1_freight = 0
+        total1_advance = 0
+        total1_balance = 0
+        total1_mt = 0
 
-    total1_freight = 0
-    total1_advance = 0
-    total1_balance = 0
-    total1_mt = 0
+        for i in builty_data:
 
-    for i in builty_data:
-
-        if not i.have_ack.filter():
+            if not i.have_ack.filter():
 
 
-            total1_balance = total1_balance + i.balance
+                total1_balance = total1_balance + i.balance
 
-        total1_freight = total1_freight + i.freight
-        total1_advance = total1_advance + i.less_advance
-        total1_mt = total1_mt + i.mt
+            total1_freight = total1_freight + i.freight
+            total1_advance = total1_advance + i.less_advance
+            total1_mt = total1_mt + i.mt
 
-    builty_filters = builty_filter(request.GET, queryset=builty_data)
+        builty_filters = builty_filter(request.GET, queryset=builty_data)
+        
+        data = builty_filters.qs
+
+
+        total_freight = 0
+        total_advance = 0
+        total_balance = 0
+        total_mt = 0
+
+        for i in data:
+
+            if not i.have_ack.filter():
+        
+                total_balance = total_balance + i.balance
+
+            total_freight = total_freight + i.freight
+            total_advance = total_advance + i.less_advance
+            total_mt = total_mt + i.mt
+
+        total_balance = round(total_balance, 2)
+        total_advance = round(total_advance, 2)
+        total_freight = round(total_freight, 2)
+        total1_freight = round(total1_freight, 2)
+        total_mt = round(total_mt, 2)
+        total1_balance = round(total1_balance, 2)
+        total1_advance = round(total1_advance, 2)
+        total1_mt = round(total1_mt, 2)
+
+
+        page = request.GET.get('page', 1)
+        paginator = Paginator(data, 20)
+
+        try:
+            data = paginator.page(page)
+        except PageNotAnInteger:
+            data = paginator.page(1)
+        except EmptyPage:
+            data = paginator.page(paginator.num_pages)
     
-    data = builty_filters.qs
-
-
-    total_freight = 0
-    total_advance = 0
-    total_balance = 0
-    total_mt = 0
-
-    for i in data:
-
-        if not i.have_ack.filter():
-       
-            total_balance = total_balance + i.balance
-
-        total_freight = total_freight + i.freight
-        total_advance = total_advance + i.less_advance
-        total_mt = total_mt + i.mt
-
-    total_balance = round(total_balance, 2)
-    total_advance = round(total_advance, 2)
-    total_freight = round(total_freight, 2)
-    total1_freight = round(total1_freight, 2)
-    total_mt = round(total_mt, 2)
-    total1_balance = round(total1_balance, 2)
-    total1_advance = round(total1_advance, 2)
-    total1_mt = round(total1_mt, 2)
-
-
-    page = request.GET.get('page', 1)
-    paginator = Paginator(data, 20)
-
-    try:
-        data = paginator.page(page)
-    except PageNotAnInteger:
-        data = paginator.page(1)
-    except EmptyPage:
-        data = paginator.page(paginator.num_pages)
-   
     context = {
         
         'data': builty_data,
-        'data' : builty_filters.qs,
         'builty_filter' : builty_filters,
         'truck_count': truck_count,
         'builty_count': builty_count,
         'user_count': user_count,
-        'user_count': user_count, 'total_freight' : total_freight,
+        'total_freight' : total_freight,
         'total_advance' : total_advance,
         'total_balance' : total_balance,
         'total1_freight' : total1_freight,
