9e8d5c02f36ff6595c18dce171afc4564df30c14
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2023-04-10 15:59:55 +0530

    weweW

--- transactions/forms.py
+++ transactions/forms.py
@@ -25,8 +25,8 @@ class builty_Form(forms.ModelForm):
                 'readonly':'readonly',
                 'class' : 'se'
             }),
-            '': forms.Select(attrs={
-                'idtruck_details': 'truck_details',
+            'truck_details': forms.Select(attrs={
+                'id' : 'truck_details',
                 'class' : 'sele',
 
             }),
