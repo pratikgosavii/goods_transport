0a510bf06714a394f57c17b45899d787668b7a9e
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2024-03-12 11:16:42 +0530

    SdS

--- transactions/filters.py
+++ transactions/filters.py
@@ -342,7 +342,6 @@ class builty_filter(django_filters.FilterSet):
 
         if not request.is_superuser:
             self.filters['district'].queryset = district.objects.filter(office_location = request.office_location)
-            self.filters['taluka'].queryset = taluka.objects.filter(office_location = request.office_location)
             self.filters['station_from'].queryset = from_station.objects.filter(office_location = request.office_location)
             self.filters['station_to'].queryset = station.objects.filter(office_location = request.office_location)
             self.filters['onaccount'].queryset = onaccount.objects.filter(office_location = request.office_location)
