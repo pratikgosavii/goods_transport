830e817f01718c29a0f60ed7b913e0ae0c60037f
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2024-04-12 17:51:39 +0530

    sdsD

--- expenses/models.py
+++ expenses/models.py
@@ -45,8 +45,8 @@ class builty_expense(models.Model):
     is_advance = models.BooleanField(default=False)
     is_porch = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    # entry_date = models.DateTimeField(default = datetime.now, blank=True, null=True)
-    entry_date = models.DateField(default=timezone.now)
+    entry_date = models.DateTimeField(default = datetime.now, blank=True, null=True)
+    # entry_date = models.DateField(default=timezone.now)
 
 
 
