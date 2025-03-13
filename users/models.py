d5f027bcb387263f8c9d6194f03f1008c64e41a2
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-03-13 18:00:03 +0530

    SdsD

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ from store.models import *
 class User(AbstractUser):
     
     company = models.ForeignKey(company, on_delete=models.CASCADE, blank = True, null = True)
-    office_location = models.ForeignKey(office_location, on_delete=models.CASCADE)
+    office_location = models.ForeignKey(office_location, on_delete=models.CASCADE, blank = True, null = True)
     password_r = models.CharField(max_length=50, null = True, blank = True)
     date_joined =  models.DateField(default=datetime.now, blank=True, null=True)
     balance = models.FloatField(default = 0)
