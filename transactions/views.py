0f6856725e36eda30081b18ada7492c8fe4cb02a
pratik gosavi <58395315+pratikgosavii@users.noreply.github.com>
2025-03-13 15:59:06 +0530

    SdsD

--- transactions/views.py
+++ transactions/views.py
@@ -1115,7 +1115,7 @@ from threading import Thread, activeCount
 from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
-import xhtml2pdf.pisa as pisa
+# import xhtml2pdf.pisa as pisa
 import os
 from random import randint
 
